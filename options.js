c3f51833e4915dc4504da55e9b7672366b0fcc9f
Abhishekanil007 <just4abhishekanil@gmail.com>
2024-02-04 00:18:46 +0530

    next commit

--- options.js
+++ options.js
@@ -1,4 +1,6 @@
 const activeCellElement = document.querySelector(".selected-cell");
+const form = document.getElementById("options-form");
+const expressionInput = document.getElementById("expression");
 
 let selectedCell = null;
 
@@ -11,3 +13,45 @@ let selectedCell = null;
     selectedCell.classList.add("active-cell");
 };
 
+function applyStylesToSelectedCell(styles){
+    selectedCell.style.fontSize = styles.fontSize + "px";
+    selectedCell.style.fontFamily = styles.fontFamily;
+    selectedCell.style.fontWeight = styles.isBold ? "bold" : "400";
+    selectedCell.style.fontStyle = styles.isItalic ? "italic" : "normal";
+    selectedCell.style.textDecoration = styles.isUnderlined ? "underline" : "none";
+    selectedCell.style.textAlign = styles.align;
+    selectedCell.style.backgroundColor = styles.backgroundColor;
+    selectedCell.style.color = styles.textColor;
+}
+
+form.addEventListener("change", function(){
+    if(!selectedCell)
+    {
+        alert("Please select a cell before making any change on the options!");
+        form.reset();
+        return;
+    }
+
+    const formData = {
+        fontFamily: form["fontFamily"].value,
+        fontSize: form["fontSize"].value,
+        isBold: form["isBold"].checked,
+        isItalic: form["isItalic"].checked,
+        isUnderlined: form["isUnderlined"].checked,
+        align: form["align"].value,
+        textColor: form["textColor"].value,
+        backgroundColor: form["backgroundColor"].value
+
+    }
+
+    applyStylesToSelectedCell(formData);
+});
+
+expressionInput.addEventListener("keyup",(e) => {
+    if(e.code === "Enter" && selectedCell)
+    {
+        let expression = expressionInput.value;
+        let result = eval(expression);
+        selectedCell.innerText = result;
+    }
+});
